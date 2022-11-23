abf08bb5520f918ce7400a977d891e8e43b68f26
PABLO DELGADO MERINO <0019880@viewnext.com>
2022-11-23 12:36:10 +0100

    Primer commit

---
+++ src/main/java/com/cursojava/proyectomilanuncios/ProyectomilanunciosApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.proyectomilanuncios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectomilanunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectomilanunciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava/proyectomilanuncios/ProyectomilanunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.proyectomilanuncios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectomilanunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
