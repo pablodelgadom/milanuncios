f52de87fbd5b2132385767fa9112070c5a268396
PABLO TRUJILLO OUAHABI <0019876@viewnext.com>
2022-11-24 16:30:01 +0100

    Añadido la funcion de ver anuncios por categoria

--- src/main/java/com/cursojava/proyectomilanuncios/controller/AnuncioController.java
+++ src/main/java/com/cursojava/proyectomilanuncios/controller/AnuncioController.java
@@ -1,19 +1,46 @@
 package com.cursojava.proyectomilanuncios.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cursojava.proyectomilanuncios.dto.AnuncioDTO;
+import com.cursojava.proyectomilanuncios.interfaces.IAnuncioService;
+import com.cursojava.proyectomilanuncios.interfaces.ICategoriaService;
+import com.cursojava.proyectomilanuncios.model.Anuncio;
+
+
+
 @Controller
 @RequestMapping("/anuncios")
 public class AnuncioController {
+	
+	@Autowired
+	IAnuncioService anuncioService;
+	
+	@Autowired
+	ICategoriaService categoriaService;
 
 	@GetMapping("/byCategoria/{id_categoria}")
 	public String AnunciosByCategoria(@PathVariable("id_categoria") int id_categoria, Model model)
 	{
-		return "";
+		List<Anuncio> anuncios = anuncioService.find_by_id_categoria(id_categoria);
+		List<AnuncioDTO> anunciosdto = new ArrayList();
+		
+		for (Anuncio anuncio : anuncios) {
+			
+			AnuncioDTO anunciodto = new AnuncioDTO(anuncio.getId_anuncio(),anuncio.getId_categoria(),anuncio.getFecha(),anuncio.getTitulo(),anuncio.getDescripcion(),anuncio.getPrecio(),anuncio.getUser());
+			anunciosdto.add(anunciodto);
+		}
+		model.addAttribute("anuncios", anunciosdto);
+		return "/listado_producto_by_categoria";
+
 	}
 	
 }
---
+++ src/main/java/com/cursojava/proyectomilanuncios/dto/AnuncioDTO.java
@@ -0,0 +1,105 @@
+package com.cursojava.proyectomilanuncios.dto;
+
+import java.sql.Date;
+
+public class AnuncioDTO {
+	
+	private int id_anuncio;
+	private int id_categoria;
+	private Date fecha;
+	private String titulo;
+	private String descripcion;
+	private double precio;
+	private String user;
+	
+	
+	public AnuncioDTO() {
+		super();
+	}
+
+
+	public AnuncioDTO(int id_anuncio, int id_categoria, Date fecha, String titulo, String descripcion, double precio,
+			String user) {
+		super();
+		this.id_anuncio = id_anuncio;
+		this.id_categoria = id_categoria;
+		this.fecha = fecha;
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.user = user;
+	}
+
+
+	public int getId_anuncio() {
+		return id_anuncio;
+	}
+
+
+	public void setId_anuncio(int id_anuncio) {
+		this.id_anuncio = id_anuncio;
+	}
+
+
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public double getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	
+	
+}
