2dc0700f8fa60d4d717eed07b1dd90bf37e8a3f6
PABLO DELGADO MERINO <0019880@viewnext.com>
2022-11-24 17:33:51 +0100

    paneladmin

--- src/main/java/com/cursojava/proyectomilanuncios/controller/AnuncioController.java
+++ src/main/java/com/cursojava/proyectomilanuncios/controller/AnuncioController.java
@@ -15,32 +15,45 @@ import com.cursojava.proyectomilanuncios.interfaces.IAnuncioService;
 import com.cursojava.proyectomilanuncios.interfaces.ICategoriaService;
 import com.cursojava.proyectomilanuncios.model.Anuncio;
 
-
-
 @Controller
 @RequestMapping("/anuncios")
 public class AnuncioController {
-	
+
 	@Autowired
 	IAnuncioService anuncioService;
-	
+
 	@Autowired
 	ICategoriaService categoriaService;
 
 	@GetMapping("/byCategoria/{id_categoria}")
-	public String AnunciosByCategoria(@PathVariable("id_categoria") int id_categoria, Model model)
-	{
+	public String AnunciosByCategoria(@PathVariable("id_categoria") int id_categoria, Model model) {
 		List<Anuncio> anuncios = anuncioService.find_by_id_categoria(id_categoria);
 		List<AnuncioDTO> anunciosdto = new ArrayList();
-		
+
 		for (Anuncio anuncio : anuncios) {
-			
-			AnuncioDTO anunciodto = new AnuncioDTO(anuncio.getId_anuncio(),anuncio.getId_categoria(),anuncio.getFecha(),anuncio.getTitulo(),anuncio.getDescripcion(),anuncio.getPrecio(),anuncio.getUser());
+
+			AnuncioDTO anunciodto = new AnuncioDTO(anuncio.getId_anuncio(), anuncio.getId_categoria(),
+					anuncio.getFecha(), anuncio.getTitulo(), anuncio.getDescripcion(), anuncio.getPrecio(),
+					anuncio.getUser());
 			anunciosdto.add(anunciodto);
 		}
 		model.addAttribute("anuncios", anunciosdto);
 		return "/listado_producto_by_categoria";
 
 	}
-	
+
+	@GetMapping("/panelAdmin")
+	public String panelAdmin(Model model) {
+		List<Anuncio> anuncios = anuncioService.list_all();
+		List<AnuncioDTO> anunciosdto = new ArrayList<AnuncioDTO>();
+		for (Anuncio anuncio : anuncios) {
+			AnuncioDTO anunciodto = new AnuncioDTO(anuncio.getId_anuncio(), anuncio.getId_categoria(),
+					anuncio.getFecha(), anuncio.getTitulo(), anuncio.getDescripcion(), anuncio.getPrecio(),
+					anuncio.getUser());
+			anunciosdto.add(anunciodto);
+		}
+		model.addAttribute("anuncios", anunciosdto);
+		return "panel_admin";
+	}
+
 }
