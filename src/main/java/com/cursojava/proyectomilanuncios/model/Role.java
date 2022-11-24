ac2e9f751ff561b20b5d4e0bb275c89a2c156a3f
PABLO TRUJILLO OUAHABI <0019876@viewnext.com>
2022-11-24 10:55:28 +0100

    Creacion de modelos user y roles

--- src/main/java/com/cursojava/proyectomilanuncios/model/Role.java
+++ src/main/java/com/cursojava/proyectomilanuncios/model/Role.java
@@ -1,5 +1,62 @@
 package com.cursojava.proyectomilanuncios.model;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "ROLES")
+
 public class Role {
+	
+	@Id
+	private String role;
+	private String funciones;
+	
+	
+	@ManyToMany()
+	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "role"), inverseJoinColumns = @JoinColumn(name = "user"))
+	private Set<Usuario> usuarios;
+	
+	public Role() {
+		super();
+		
+	}
+
+
+	public Role(String role, String funciones) {
+		super();
+		this.role = role;
+		this.funciones = funciones;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public String getFunciones() {
+		return funciones;
+	}
+
+
+	public void setFunciones(String funciones) {
+		this.funciones = funciones;
+	}
+	
+	
+	
 
 }
--- src/main/java/com/cursojava/proyectomilanuncios/model/Usuario.java
+++ src/main/java/com/cursojava/proyectomilanuncios/model/Usuario.java
@@ -1,5 +1,76 @@
 package com.cursojava.proyectomilanuncios.model;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "USUARIOS")
+
 public class Usuario {
+	
+	@Id
+	private String user;
+	private String email;
+	private String password;
+	
+	@ManyToMany()
+	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "user"), inverseJoinColumns = @JoinColumn(name = "role"))
+	private Set<Role> roles;
+	
+	
+	public Usuario() {
+		super();
+	}
+
+
+	public Usuario(String user, String email, String password) {
+		super();
+		this.user = user;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+	
 
 }
