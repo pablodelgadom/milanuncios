96582751c100f5b03ebdd4d7b44f70105d2bb775
PABLO DELGADO MERINO <0019880@viewnext.com>
2022-11-24 10:56:17 +0100

    models

--- src/main/java/com/cursojava/proyectomilanuncios/model/Anuncio.java
+++ src/main/java/com/cursojava/proyectomilanuncios/model/Anuncio.java
@@ -1,5 +1,112 @@
 package com.cursojava.proyectomilanuncios.model;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ANUNCIOS")
 public class Anuncio {
+
+	@Id
+	private int id_anuncio;
+	private int id_categoria;
+	private Date fecha;
+	private String titulo;
+	private String descripcion;
+	private double precio;
+	private String user;
+
+	@ManyToOne
+	@JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria", insertable = false, updatable = false)
+	private Categoria categoria;
+	
+	@ManyToOne
+	@JoinColumn(name = "user", referencedColumnName = "user", insertable = false, updatable = false)
+	private Usuario usuario;
+
+	public Anuncio() {
+		super();
+	}
+
+	public Anuncio(int id_anuncio, int id_categoria, Date fecha, String titulo, String descripcion, double precio,
+			String user) {
+		super();
+		this.id_anuncio = id_anuncio;
+		this.id_categoria = id_categoria;
+		this.fecha = fecha;
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.user = user;
+	}
+
+	public int getId_anuncio() {
+		return id_anuncio;
+	}
+
+	public void setId_anuncio(int id_anuncio) {
+		this.id_anuncio = id_anuncio;
+	}
+
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 }
--- src/main/java/com/cursojava/proyectomilanuncios/model/Categoria.java
+++ src/main/java/com/cursojava/proyectomilanuncios/model/Categoria.java
@@ -1,5 +1,72 @@
 package com.cursojava.proyectomilanuncios.model;
 
-public class Categoria {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CATEGORIAS")
+public class Categoria {
+	@Id
+	@Column(name = "id_categoria")
+	private int id_categoria;
+	@Column(name = "descripcion")
+	private String descripcion;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
+	private List<Anuncio> anuncios;
+
+	public Categoria() {
+		super();
+
+	}
+
+
+
+	public Categoria(int id_categoria, String descripcion, List<Anuncio> anuncios) {
+		super();
+		this.id_categoria = id_categoria;
+		this.descripcion = descripcion;
+		this.anuncios = anuncios;
+	}
+
+
+
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+	public List<Anuncio> getAnuncios() {
+		return anuncios;
+	}
+
+
+
+	public void setAnuncios(List<Anuncio> anuncios) {
+		this.anuncios = anuncios;
+	}
+
+	
+	
 }
