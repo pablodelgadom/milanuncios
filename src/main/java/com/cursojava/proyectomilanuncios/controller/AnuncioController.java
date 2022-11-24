3e138970ac2307c1dbc0d7243762909d94b2e5cc
PABLO DELGADO MERINO <0019880@viewnext.com>
2022-11-24 15:15:40 +0100

    a

--- src/main/java/com/cursojava/proyectomilanuncios/controller/AnuncioController.java
+++ src/main/java/com/cursojava/proyectomilanuncios/controller/AnuncioController.java
@@ -1,5 +1,19 @@
 package com.cursojava.proyectomilanuncios.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/anuncios")
 public class AnuncioController {
 
+	@GetMapping("/byCategoria/{id_categoria}")
+	public String AnunciosByCategoria(@PathVariable("id_categoria") int id_categoria, Model model)
+	{
+		return "";
+	}
+	
 }
