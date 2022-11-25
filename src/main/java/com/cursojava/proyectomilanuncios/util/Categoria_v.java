d9ea144d0b36c10ae017b1dd29830f3b691a2648
PABLO DELGADO MERINO <0019880@viewnext.com>
2022-11-25 11:38:12 +0100

    admin3

---
+++ src/main/java/com/cursojava/proyectomilanuncios/util/Categoria_v.java
@@ -0,0 +1,46 @@
+package com.cursojava.proyectomilanuncios.util;
+
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+
+public class Categoria_v {
+
+	private int id_categoria;
+	private String descripcion;
+
+	public Categoria_v() {
+		super();
+
+	}
+
+	public Categoria_v(int id_categoria, String descripcion) {
+		super();
+		this.id_categoria = id_categoria;
+		this.descripcion = descripcion;
+	}
+
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public boolean validate(Errors errors) {
+		if (!StringUtils.hasText(descripcion)) {
+			errors.rejectValue("descripcion", "badFormat", "Rellene el user");
+		}
+
+		return errors.hasErrors();
+	}
+
+}
